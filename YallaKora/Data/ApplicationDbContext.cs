3f96c080e1c31bb62e5d2700a1f72d5a255bf2d1
andre <andre@DESKTOP-2N3NSL0>
2023-09-12 16:28:22 +0300

    Add project files.

---
+++ YallaKora/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using YallaKora.Models;
+
+namespace YallaKora.Data
+{
+	public class ApplicationDbContext : IdentityDbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+			: base(options)
+		{
+		}
+		public DbSet<YallaKora.Models.Question> Question { get; set; } = default!;
+	}
+}
---
+++ YallaKora/Models/Question.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+
+namespace YallaKora.Models
+{
+	public class Question
+	{
+		[Key]
+		public int Id { get; set; }
+		[Required]
+		public string QuestionInfo { get; set; }
+
+		public string Answer { get; set; }
+
+        
+
+    }
+}
